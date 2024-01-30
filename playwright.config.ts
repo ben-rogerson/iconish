dcb2813728ea877b3025a8b25bdf3bfaf6849638
Ben Rogerson <info@benrogerson.dev>
2024-01-31 09:06:10 +1030

    No check playwright cfg

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { defineConfig, devices } from '@playwright/experimental-ct-react'
 import { plugins, resolve } from './vitest.config'
 
