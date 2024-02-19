c15874f1851449c5f00c7797959a69f8aea011fb
Ben Rogerson <info@benrogerson.dev>
2024-02-19 12:06:50 +1030

    Switch to dark bg for underlay

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,7 @@ export default {
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
+        'background-sheet': 'hsl(var(--background-sheet))',
         foreground: 'hsl(var(--foreground))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
