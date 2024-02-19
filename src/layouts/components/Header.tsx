6d1b197bf0d6eb0328c436aea5d4eab901d3a743
Ben Rogerson <info@benrogerson.dev>
2024-02-19 12:11:41 +1030

    Add intro

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -8,7 +8,22 @@ export function Header() {
         {logoIcon}
         <div>Iconish</div>
       </Link>
-      <h1>align and optimize your svg icons</h1>
+      <div className="mb-10 mt-12 space-y-6 rounded border p-10">
+        <h1 className="text-[200%] font-semibold leading-tight tracking-tight">
+          Align and minify your icon sets for the web
+        </h1>
+        <div className="max-w-[600px] space-y-4">
+          <p>
+            Exporting icons as SVG from tools like Figma can be hit or miss. The
+            code is messy, and colors may not match.
+          </p>
+          <p>
+            With Iconish, you can quickly tidy and sync your icon sets. Align
+            colors, refine paths, and shrink code size for the best possible
+            finish.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
