b006adf456c310b4f9b4cd95d6967d6541503259
Ben Rogerson <info@benrogerson.dev>
2024-02-19 12:06:09 +1030

    Switch to google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,24 @@
 import type { Metadata } from 'next'
 import { ThemeScript } from 'next-app-theme/theme-script'
+import { Chivo_Mono } from 'next/font/google'
 
 import './globals.css'
 
+// If loading a variable font, you don't need to specify the font weight
+const font = Chivo_Mono({
+  weight: ['500', '300'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export const metadata: Metadata = {
   title: 'Iconish',
-  description: 'Align and optimize SVG icons for the web',
+  description: 'Align and minify SVG icons for the web',
 }
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className={font.className} suppressHydrationWarning>
       <head>
         <ThemeScript />
       </head>
