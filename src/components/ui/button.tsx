189290f058bd0ef2a91b8c01316bd025512bfa6a
Ben Rogerson <info@benrogerson.dev>
2024-02-19 12:50:31 +1030

    Fix invisi issue with types

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
