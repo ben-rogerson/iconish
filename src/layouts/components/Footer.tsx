48538371750d5ac008594deb30d9d4846d836452
Ben Rogerson <info@benrogerson.dev>
2024-02-20 21:32:37 +1030

    Add GitHub link to footer

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -2,7 +2,16 @@ export function Footer() {
   return (
     <div className="mt-auto">
       <div className="mt-20 flex justify-between gap-5 border-t pb-10 pt-10 text-muted">
-        <div className="text-center">&copy; 2024 Iconish</div>
+        <div>&copy; 2024 Iconish</div>
+        <div>
+          <a
+            href="https://github.com/ben-rogerson/iconish"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
     </div>
   )
