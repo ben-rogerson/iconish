200a4395d9a64f78844c874a4594b6cd5ab4f709
Ben Rogerson <info@benrogerson.dev>
2024-02-21 08:44:56 +1030

    Add GH issue/request link

--- src/layouts/components/Footer.tsx
+++ src/layouts/components/Footer.tsx
@@ -1,15 +1,22 @@
 export function Footer() {
   return (
     <div className="mt-auto">
-      <div className="mt-20 flex justify-between gap-5 border-t pb-10 pt-10 text-muted">
+      <div className="mt-20 flex justify-between gap-7 border-t pb-10 pt-10 text-muted">
         <div>&copy; 2024 Iconish</div>
-        <div>
+        <div className="flex gap-7">
           <a
             href="https://github.com/ben-rogerson/iconish"
             target="_blank"
             rel="noreferrer"
           >
             GitHub
+          </a>
+          <a
+            href="https://github.com/ben-rogerson/iconish/issues/new"
+            target="_blank"
+            rel="noreferrer"
+          >
+            New issue/request
           </a>
         </div>
       </div>
